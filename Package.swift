1536377e66ea6f060178276d439cee8edd71efe8
VlaStoBIT <vladica.stojic@bit.admin.ch>
2025-04-23 17:49:09 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "JsonSchemaValidator",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "JsonSchemaValidator",targets:["JsonSchemaValidatorSources"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "JsonSchemaValidatorWrapper",
+            dependencies:[
+                .target(name:"JsonSchemaValidator", condition: .when(platforms: [.iOS]))
+            ],
+            path: "JsonSchemaValidatorWrapper"
+        ),
+        .binaryTarget(
+            name: "JsonSchemaValidator",
+            path: "./jsonschemavalidator.xcframework"
+        ),
+        .target(
+            name: "JsonSchemaValidatorSources",
+            dependencies: ["JsonSchemaValidatorWrapper"],
+            path: "swift-sources"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,15 @@
+![Public Beta banner](https://github.com/swiyu-admin-ch/swiyu-admin-ch.github.io/blob/main/assets/images/github-banner.jpg)
+
+# jsonschema (validator) for Swift applications
+
+This project contains language bindings required for loading and using the [jsonschema](https://github.com/swiyu-admin-ch/jsonschema-bindgen) library in Swift applications.
+
+You are more then welcome to explore the [relevant examples](https://github.com/swiyu-admin-ch/jsonschema-examples) in a further detail.
+
+## Contributions and feedback
+
+We welcome any feedback on the code regarding both the implementation and security aspects. Please follow the guidelines for contributing found in [CONTRIBUTING](./CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the terms of the MIT license. See the [LICENSE](./LICENSE.md) file for details.
