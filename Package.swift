e8e58462b4251c44ab4656982a5ef2018d24e096
VladiStoBIT <vladica.stojic@bit.admin.ch>
2025-09-24 23:41:20 +0200

    ✨ Bump version 0.30.0 (binary Swift package distribution)

--- Package.swift
+++ Package.swift
@@ -1,31 +1,31 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let version = "0.30.0"
+let xcframework_name = "jsonschemavalidator"
+let binary_target_url_github_owner = "swiyu-admin-ch"
+let binary_target_url_github_repo = "jsonschema-swift"
+let checksum = "ae7616ebd4a14c23153619a0491ecae81e2f05c9ba3d34a1c88099d7a1742333"
+
 let package = Package(
     name: "JsonSchemaValidator",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "JsonSchemaValidator",targets:["JsonSchemaValidatorSources"]),
+        .library(
+            name: "JsonSchemaValidator",
+            targets: ["JsonSchemaValidator", "JsonSchemaValidatorRemoteBinaryPackage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "JsonSchemaValidatorWrapper",
-            dependencies:[
-                .target(name:"JsonSchemaValidator", condition: .when(platforms: [.iOS]))
-            ],
-            path: "JsonSchemaValidatorWrapper"
+            name: "JsonSchemaValidator"
         ),
         .binaryTarget(
-            name: "JsonSchemaValidator",
-            path: "./jsonschemavalidator.xcframework"
-        ),
-        .target(
-            name: "JsonSchemaValidatorSources",
-            dependencies: ["JsonSchemaValidatorWrapper"],
-            path: "swift-sources"
+            name: "JsonSchemaValidatorRemoteBinaryPackage",
+            url: "https://github.com/\(binary_target_url_github_owner)/\(binary_target_url_github_repo)/releases/download/\(version)/\(xcframework_name)-\(version).xcframework.zip",
+            checksum: "\(checksum)"
         )
     ]
 )
